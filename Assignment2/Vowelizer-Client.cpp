ab581914f2ba0da93406c40bf07e102f21e993ab
Sharjeel Junaid <sharjeelj@outlook.com>
2022-02-11 03:06:00 -0700

    Updated Vowelizer-Server - Able to send a static response over TCP & UDP after receiving data over TCP then UDP from the client - Requires UDP timeouts and ideally a better way to handle UDP sending (maybe send client's port # over TCP and use the TCP handshake IP) - WIP and comments are likely not accurate
    Updated Vowelizer-Client
    - Able to send a user defined message over TCP after which gets a response from the server over TCP & UDP
    - Requires UDP timeouts
    - WIP and comments are likely not accurate
    
    Removed Vowelizer.xml
    - Code for this runConfig was removed previously

--- Assignment2/Vowelizer-Client.cpp
+++ Assignment2/Vowelizer-Client.cpp
@@ -1,51 +1,174 @@
-// Client side implementation of UDP client-server model
-#include <stdio.h>
-#include <stdlib.h>
+/* This file contains modified code from the following source(s):
+ * https://www.binarytides.com/socket-programming-c-linux-tutorial/ (Socket Programming in C on Linux provided by the Professor under Useful Links on the course page)
+ * Client.cpp from T02/T03 Week1
+ * Server.cpp from T02/T03 Week1
+*/
+
+#include <cstdio>
+#include <cstring>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 
-#define PORT     48259
-#define MAXLINE 1024
-//#define MSG_CONFIRM 0
+// Global static variables
+#define IP "127.0.0.1"
+#define PORT 48259
+#define MAX_MESSAGE_SIZE 1024
+#define EXIT_VALUE 0
+#define DEVOWEL_VALUE 1
+#define ENVOWEL_VALUE 2
 
-// Driver code
+void printMenu() {
+    printf("\n");
+    printf("Please choose an option from the following menu:\n");
+    printf("  0 - Exit program\n");
+    printf("  1 - Devowel a message\n");
+    printf("  2 - Envowel a message\n");
+    printf("Your desired menu selection? ");
+}
+
+// Main function
 int main() {
-    int sockfd;
-    char buffer[MAXLINE];
-//    char *hello = "Hello from client";
-    char *hello = "Hello there!";
-    struct sockaddr_in servaddr;
+    // Creates a struct that will store the complete address of the socket
+    struct sockaddr_in socketAddress;
 
-    // Creating socket file descriptor
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
+    // Creates char arrays that will store the message between the client and the server
+    char tcpCompleteMessage[MAX_MESSAGE_SIZE];
+    char tcpSplitMessage[MAX_MESSAGE_SIZE];
+    char udpSplitMessage[MAX_MESSAGE_SIZE];
+
+    // Initialize int variables to store the number of bytes each char array holds
+    int tcpCompleteMessageBytes = 0;
+    int tcpSplitMessageBytes = 0;
+    int udpSplitMessageBytes = 0;
+
+    // Allocates memory to the location that will store the address of the socket
+    memset(&socketAddress, 0, sizeof(socketAddress));
+
+    // Sets the socket address family to IPv4
+    socketAddress.sin_family = AF_INET;
+
+    // Sets the port number that the socket will use
+    socketAddress.sin_port = htons(PORT);
+
+    // Sets the complete IP address that the socket will use
+    inet_pton(AF_INET, IP, &socketAddress.sin_addr);
+
+    // Creates a TCP socket that will be used to communicate with the server otherwise prints an error and returns if unsuccessful
+    int customSocketTCP = socket(AF_INET, SOCK_STREAM, 0);
+    if (customSocketTCP == -1) {
+        printf("TCP Socket Creation Failed!\n");
+        return -1;
     }
 
-    memset(&servaddr, 0, sizeof(servaddr));
+    // Creates a UDP socket that will be used to communicate with the server otherwise prints an error and returns if unsuccessful
+    int customSocketUDP = socket(AF_INET, SOCK_DGRAM, 0);
+    if (customSocketUDP == -1) {
+        printf("UDP Socket Creation Failed!\n");
+        return -1;
+    }
 
-    // Filling server information
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = INADDR_ANY;
+    // Establishes a connection using the TCP socket otherwise prints an error and returns if unsuccessful
+    int connectionStatusTCP = connect(customSocketTCP, (struct sockaddr *) &socketAddress, sizeof(struct sockaddr_in));
+    if (connectionStatusTCP == -1) {
+        printf("TCP Socket Connection Failed!\n");
+        return -1;
+    }
 
-    int n, len;
+    // Establishes a connection using the UDP socket otherwise prints an error and returns if unsuccessful
+    int connectionStatusUDP = connect(customSocketUDP, (struct sockaddr *) &socketAddress, sizeof(struct sockaddr_in));
+    if (connectionStatusUDP == -1) {
+        printf("UDP Socket Connection Failed!\n");
+        return -1;
+    }
 
-    sendto(sockfd, (const char *) hello, strlen(hello),
-           MSG_CONFIRM, (const struct sockaddr *) &servaddr,
-           sizeof(servaddr));
-    printf("Hello message sent.\n");
+    int userMenuSelection;
+    char userInput;
 
-    n = recvfrom(sockfd, (char *) buffer, MAXLINE,
-                 MSG_WAITALL, (struct sockaddr *) &servaddr,
-                 (socklen_t *) &len);
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer);
+    while (userMenuSelection != EXIT_VALUE) {
+        printMenu();
+        scanf("%d", &userMenuSelection);
 
-    close(sockfd);
+        if (userMenuSelection == DEVOWEL_VALUE) {
+            // Gets the newline character that follows the user's menu selection input
+            userInput = getchar();
+
+            // Gets and stores the message that needs to be sent to the server from the user
+            printf("Enter your message: ");
+            tcpCompleteMessageBytes = 0;
+            while ((userInput = getchar()) != '\n') {
+                tcpCompleteMessage[tcpCompleteMessageBytes] = userInput;
+                tcpCompleteMessageBytes++;
+            }
+
+            // Null terminates the message
+            tcpCompleteMessage[tcpCompleteMessageBytes] = '\0';
+
+            // Sends the message using the socket otherwise prints an error and returns if unsuccessful
+            tcpCompleteMessageBytes = send(customSocketTCP, tcpCompleteMessage, strlen(tcpCompleteMessage), 0);
+            if (tcpCompleteMessageBytes == -1) {
+                printf("TCP Socket Send Failed!\n");
+                return -1;
+            }
+
+//            printf("I just sent %d bytes to the server via the TCP socket!\n", tcpCompleteMessageBytes);
+
+            // Receives the reply using the socket otherwise prints an error and returns if unsuccessful
+            tcpSplitMessageBytes = recv(customSocketTCP, tcpSplitMessage, MAX_MESSAGE_SIZE, 0);
+            if (tcpSplitMessageBytes == -1) {
+                printf("TCP Socket Receive Failed!\n");
+                return -1;
+            }
+
+//            printf("I just got %d bytes from the server via the TCP socket!\n", tcpSplitMessageBytes);
+
+            if (tcpSplitMessageBytes > 0) {
+                /* make sure the message is null-terminated in C */
+                tcpSplitMessage[tcpSplitMessageBytes] = '\0';
+                printf("Answer from server: ");
+                printf("`%s'\n", tcpSplitMessage);
+            } else {
+                /* an error condition if the server ends unexpectedly */
+                printf("TCP Socket Received Nothing!\n");
+                continue;
+            }
+
+            sendto(customSocketUDP, "TEST", strlen("TEST"),
+                   MSG_CONFIRM, (const struct sockaddr *) &socketAddress,
+                   sizeof(socketAddress));
+
+            // Receives the reply using the socket otherwise prints an error and returns if unsuccessful
+            int len;
+            udpSplitMessageBytes = recvfrom(customSocketUDP, udpSplitMessage, MAX_MESSAGE_SIZE, MSG_WAITALL,
+                                            (struct sockaddr *) &socketAddress, (socklen_t *) &len);
+            if (udpSplitMessageBytes == -1) {
+                printf("UDP Socket Receive Failed!\n");
+                return -1;
+            }
+
+//            printf("I just got %d bytes from the server via the UDP socket!\n", tcpSplitMessageBytes);
+
+            if (udpSplitMessageBytes > 0) {
+                /* make sure the message is null-terminated in C */
+                udpSplitMessage[udpSplitMessageBytes] = '\0';
+                printf("Answer from server: ");
+                printf("`%s'\n", udpSplitMessage);
+            } else {
+                /* an error condition if the server ends unexpectedly */
+                printf("UDP Socket Received Nothing!\n");
+                continue;
+            }
+        } else if (userMenuSelection == ENVOWEL_VALUE) {
+
+        } else if (userMenuSelection != EXIT_VALUE)
+            printf("Invalid menu selection. Please try again.\n");
+    }
+
+    // Closes the sockets
+    close(customSocketTCP);
+    close(customSocketUDP);
+
+    // Ends the program with the success exit code
     return 0;
 }
--- Assignment2/Vowelizer-Server.cpp
+++ Assignment2/Vowelizer-Server.cpp
@@ -1,99 +1,164 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* This file contains modified code from the following source(s):
+ * https://www.binarytides.com/socket-programming-c-linux-tutorial/ (Socket Programming in C on Linux provided by the Professor under Useful Links on the course page)
+ * http://pages.cpsc.ucalgary.ca/~carey/CPSC441/ass0/mypal-server.c (Professor's palindrome checking server)
+*/
+
+#include <cstdio>
+#include <cstdlib>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <cstring>
 #include <arpa/inet.h>
-#include <netinet/in.h>
 
+// Global static variables
+#define MAX_MESSAGE_SIZE 1024
+#define PORT 48259
 
-#define PORT     48259
-#define MAXLINE 1024
-//#define MSG_CONFIRM 0
+// Creates a socket that will be used by the fork to allow the client to communicate with the proxy
+int clientSocketTCP;
 
-// Driver code
+// Main function
 int main() {
-    int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from server";
-    struct sockaddr_in servaddr, cliaddr;
+    // Creates a struct that will store the complete address of the socket being created
+    struct sockaddr_in socketAddress = {};
+    struct sockaddr_in clientAddress = {};
 
-    // Creating socket file descriptor
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
+    // Creates char arrays that will store the message between the client and the server
+    char tcpCompleteMessage[MAX_MESSAGE_SIZE];
+    char tcpSplitMessage[MAX_MESSAGE_SIZE];
+    char udpSplitMessage[MAX_MESSAGE_SIZE];
+
+    // Initialize int variables to store the number of bytes each char array holds
+    int tcpCompleteMessageBytes = 0;
+    int tcpSplitMessageBytes = 0;
+    int udpSplitMessageBytes = 0;
+
+    // Initializes the incoming and outgoing message and temporary buffer arrays with zeroed bytes
+    memset(tcpCompleteMessage, 0, MAX_MESSAGE_SIZE);
+    memset(tcpSplitMessage, 0, MAX_MESSAGE_SIZE);
+    memset(udpSplitMessage, 0, MAX_MESSAGE_SIZE);
+
+    // Creates a socket that will be used by the client to initially communicate with the proxy
+    int serverSocketTCP;
+    int serverSocketUDP;
+
+    // Will be used to store the PID of a child fork
+    pid_t pid;
+
+    // Allocates memory to the location that will store the address of the socket
+    memset(&socketAddress, 0, sizeof(socketAddress));
+    memset(&clientAddress, 0, sizeof(clientAddress));
+
+    // Sets the socket address family to IPv4
+    socketAddress.sin_family = AF_INET;
+
+    // Sets the port number that the socket will use
+    socketAddress.sin_port = htons(PORT);
+
+    // Sets the complete IP address that the socket will use
+    socketAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // Creates a socket that will be used to communicate between the server and the client otherwise prints an error and returns if unsuccessful
+    serverSocketTCP = socket(PF_INET, SOCK_STREAM, 0);
+    if (serverSocketTCP == -1) {
+        fprintf(stderr, "Server: TCP socket() failed!\n");
+        exit(1);
     }
 
-    memset(&servaddr, 0, sizeof(servaddr));
-    memset(&cliaddr, 0, sizeof(cliaddr));
-
-    // Filling server information
-    servaddr.sin_family = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(PORT);
-
-    // Bind the socket with the server address
-    if (bind(sockfd, (const struct sockaddr *) &servaddr,
-             sizeof(servaddr)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    // Creates a socket that will be used to communicate between the server and the client otherwise prints an error and returns if unsuccessful
+    serverSocketUDP = socket(PF_INET, SOCK_DGRAM, 0);
+    if (serverSocketUDP == -1) {
+        fprintf(stderr, "Server: UDP socket() failed!\n");
+        exit(1);
     }
 
-    int len, n;
+    // Binds the specific address and port number to the socket otherwise prints an error and returns if unsuccessful
+    if (bind(serverSocketTCP, (struct sockaddr *) &socketAddress, sizeof(struct sockaddr_in)) == -1) {
+        fprintf(stderr, "Server: TCP bind() failed!\n");
+        exit(1);
+    }
 
-    len = sizeof(cliaddr);  //len is value/resuslt
+    // Binds the specific address and port number to the socket otherwise prints an error and returns if unsuccessful
+    if (bind(serverSocketUDP, (struct sockaddr *) &socketAddress, sizeof(struct sockaddr_in)) == -1) {
+        fprintf(stderr, "Server: UDP bind() failed!\n");
+        exit(1);
+    }
 
-    n = recvfrom(sockfd, (char *) buffer, MAXLINE,
-                 MSG_WAITALL, (struct sockaddr *) &cliaddr,
-                 (socklen_t *) &len);
-    buffer[n] = '\0';
-    printf("Client : %s\n", buffer);
-    sendto(sockfd, (const char *) hello, strlen(hello),
-           MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
-           len);
-    printf("Hello message sent.\n");
+    // Listens on the socket for an incoming client connection
+    if (listen(serverSocketTCP, 5) == -1) {
+        fprintf(stderr, "Server: TCP listen() failed!\n");
+        exit(1);
+    }
 
-    printf("\nTEST: \n");
-    char vowels[n];
-    char rest[n];
-    memset(vowels, ' ', n);
-    memset(rest, ' ', n);
-    for (int counter = 0; counter < n; counter++) {
-        char *characterStatus;
-        switch (buffer[counter]) {
-            case 'A':
-            case 'E':
-            case 'I':
-            case 'O':
-            case 'U':
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-                characterStatus = "Vowel";
-                vowels[counter] = buffer[counter];
-                break;
-            default:
-                characterStatus = "Other";
-                rest[counter] = buffer[counter];
-                break;
+    // Prints out the welcome message and TCP & UDP port number that the proxy is operating on
+    fprintf(stderr, "Server running on TCP & UDP port %d...\n\n", PORT);
+
+    // Loops forever to handle client requests
+    while (true) {
+        // Accepts an incoming socket connection request from the client
+        clientSocketTCP = accept(serverSocketTCP, NULL, NULL);
+        if (clientSocketTCP == -1) {
+            fprintf(stderr, "Server: TCP accept() failed!\n");
+            exit(1);
         }
-        printf("%c (%d): %s\n", buffer[counter], buffer[counter], characterStatus);
-    }
-    printf("Vowels: %s\n", vowels);
-    printf("Rest: %s\n", rest);
 
-    char merged[n];
-    memset(merged, ' ', n);
-    for (int counter = 0; counter < n; counter++) {
-        if (vowels[counter] == ' ' && rest[counter] != ' ')
-            merged[counter] = rest[counter];
-        else if (vowels[counter] != ' ' && rest[counter] == ' ')
-            merged[counter] = vowels[counter];
-    }
-    printf("Merged: %s\n", merged);
+        // Creates a fork that will deal with the client
+        pid = fork();
 
-    return 0;
+        // Only proceeds further if the fork creation was successful (0 is returned)
+        if (pid < 0) {
+            fprintf(stderr, "Server: fork() failed!\n");
+            exit(1);
+        } else if (pid == 0) {
+            // Closes the inherited socket from the parent as the child does not need it
+            close(serverSocketTCP);
+
+            // Receives the client's message using the socket
+            bzero(tcpCompleteMessage, MAX_MESSAGE_SIZE);
+            tcpCompleteMessageBytes = recv(clientSocketTCP, tcpCompleteMessage, MAX_MESSAGE_SIZE, 0);
+
+            while (tcpCompleteMessageBytes > 0) {
+                // Sends the server's reply to the client
+                sprintf(tcpSplitMessage, "TCP REPLY");
+                tcpSplitMessageBytes = strlen(tcpSplitMessage);
+                if (send(clientSocketTCP, tcpSplitMessage, tcpSplitMessageBytes, 0) < 0) {
+                    fprintf(stderr, "Server: TCP send() failed!\n");
+                    exit(1);
+                }
+
+                int len = sizeof(clientAddress);
+                char test[MAX_MESSAGE_SIZE];
+                recvfrom(serverSocketUDP, test, MAX_MESSAGE_SIZE,
+                         MSG_WAITALL, (struct sockaddr *) &clientAddress,
+                         (socklen_t *) &len);
+
+                // Sends the server's reply to the client
+                sprintf(udpSplitMessage, "UDP REPLY");
+                udpSplitMessageBytes = strlen(udpSplitMessage);
+                if (sendto(serverSocketUDP, udpSplitMessage, udpSplitMessageBytes,
+                           MSG_CONFIRM, (const struct sockaddr *) &clientAddress,
+                           len) < 0) {
+                    fprintf(stderr, "Server: UDP send() failed!\n");
+                    exit(1);
+                }
+
+                bzero(tcpCompleteMessage, MAX_MESSAGE_SIZE);
+                bzero(tcpSplitMessage, MAX_MESSAGE_SIZE);
+                bzero(udpSplitMessage, MAX_MESSAGE_SIZE);
+
+                tcpCompleteMessageBytes = recv(clientSocketTCP, tcpCompleteMessage, MAX_MESSAGE_SIZE, 0);
+            }
+
+            // Closes the socket with the client and kills the fork
+            fprintf(stderr, "Child process received nothing, so it is exiting now\n");
+            close(clientSocketTCP);
+            exit(0);
+        } else {
+            // Informs the user of the child's PID (that will be handling the client and proxy connection) and that the proxy server will now continue to listen for new requests
+            fprintf(stderr, "Proxy Server created child process %d to handle the client\n", pid);
+            fprintf(stderr, "Proxy Server process going back to listening for new clients now...\n\n");
+
+            // Parent does not need the socket that communicates with the client
+            close(clientSocketTCP);
+        }
+    }
 }
